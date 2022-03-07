52c38b01d518a6b344df53acbd4a41e9f69973cf
Marta <mswolinska@gmail.com>
2022-03-07 14:43:53 +0000

    Check if user input position is available
    Checks if the position typed by the user has already been played or not.

--- player.py
+++ player.py
@@ -34,8 +34,23 @@ def play_move(board: List[List], player_number):
     else:
         return play_move(board, player_number)
 
+def list_available_positions(board: np.ndarray) -> List[bool]:
+    avail_positions = []
+    current_board = board.flatten()
+    for el in current_board:
+        if el == 0:
+            avail_positions.append(True)
+        else:
+            avail_positions.append(False)
+    return avail_positions
 
 def is_position_available(board: np.ndarray, row: int, column: int) -> bool:
+    available_positions = list_available_positions(board)
+    position_index = row * 3 + column
     if row > 2 or column > 2:
         print("Position doesn't exist, pick another one")
         return False
+
+    if not available_positions[position_index]:
+        print("Position is not available, try another one")
+    return available_positions[position_index]
