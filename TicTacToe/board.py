186ab4990383a765195dbf357167bc791ae0e811
Marta <mswolinska@gmail.com>
2022-03-18 17:18:54 +0000

    Improved probability matrix to account for opponent wins
    Probability matrix now takes into account the number of steps needed to get to the final outcome. It also now considers if not making a step will lead to an opponent win i.e. a loss. 
    Fixed bug in definition of a draw

--- TicTacToe/board.py
+++ TicTacToe/board.py
@@ -102,17 +102,24 @@ class BoardSimulation(object):
             row = first_move[0]
             column = first_move[1]
             self.board[row][column] = self.player_number
+            # opponent_win_in_one_move = False
 
             interim_board = self.board.copy()
 
             all_sequences = self.get_all_move_permutations()
 
             for game_sequence in all_sequences:
-                self.get_game_end_board(game_sequence)
-                score_for_move += self.get_game_outcome_score()
+                number_moves_till_outcome = self.get_game_end_board(game_sequence)
+                if number_moves_till_outcome != 0:
+                    score_for_move += (self.get_game_outcome_score() / number_moves_till_outcome)
+                else:
+                    score_for_move += self.get_game_outcome_score()
                 self.board = interim_board.copy()
 
-            win_probability_array[row][column] = score_for_move / len(all_sequences)
+            # if opponent_win_in_one_move:
+            #     win_probability_array[row][column] = 1
+            else:
+                win_probability_array[row][column] = score_for_move / len(all_sequences)
             self.board = starting_board.copy()
 
         print(win_probability_array)
@@ -125,24 +132,30 @@ class BoardSimulation(object):
 
     def get_game_end_board(self, move_sequence):
         current_player = self.opponent_player_number
-        game_board = self.board
+        n_moves_till_end = 0
 
         for i in range(len(move_sequence)):
             row = move_sequence[i][0]
             column = move_sequence[i][1]
-            game_board[row][column] = current_player
+            self.board[row][column] = current_player
+            n_moves_till_end = i + 1
+
+            if self.is_simulation_win(current_player):
+                return n_moves_till_end
+
             current_player = self.change_player(current_player)
 
-        return game_board
+        return n_moves_till_end
 
     def get_game_outcome_score(self):
-        if self.is_game_draw():
+        if self.is_simulation_win(self.player_number):
+            return 1
+        elif self.is_simulation_win(self.opponent_player_number):
+            return -1
+        elif self.is_game_draw():
             return 0
         else:
-            if self.is_simulation_win():
-                return 1
-            else:
-                return -1
+            return None
 
     def change_player(self, player):
         if player == self.player_number:
@@ -165,8 +178,8 @@ class BoardSimulation(object):
                 avail_position_tuples.append((row, column))
         return avail_position_tuples
 
-    def is_simulation_win(self):
-        is_win_mask = self.board == self.player_number
+    def is_simulation_win(self, current_player):
+        is_win_mask = self.board == current_player
 
         for i in range(3):
             # is there a win in each row?
@@ -184,16 +197,16 @@ class BoardSimulation(object):
         return False
 
     def is_game_draw(self) -> bool:
-        if self.number_of_moves_remaining().all() == 0 and not self.is_simulation_win():
-            return True
-        else:
+        if self.number_of_moves_remaining().any():
             return False
+        else:
+            return True
 
 if __name__ == '__main__':
     a = np.array(
-         [[0, 0, 0],
-         [0, 0, 0],
-         [0, 0, 0]])
+         [[1, 2, 1],
+         [2, 2, 1],
+         [0, 1, 0],])
 
     tic = time.perf_counter()
     game_sim = BoardSimulation(a)
