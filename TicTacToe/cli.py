99643c0cb8decc992d7f46fcd93e81a3974e709e
Marta <mswolinska@gmail.com>
2022-04-11 12:19:50 +0100

    Update cli to use poetry scripts instead.
    Remove the previous implementation where to play the game, the user would use the play_game bash script. This is substituted with poetry scripts. 
    Additionally, there are now two options to play the game: either against a bot or against another human.

--- TicTacToe/cli.py
+++ TicTacToe/cli.py
@@ -1,8 +1,14 @@
 import TicTacToe
 
-if __name__ == '__main__':
+def one_player_game():
     my_game = TicTacToe.TicTacToe(
         number_of_human_players=1,
         number_of_random_players=1,
+    )
+    my_game.play_game()
+
+def two_player_game():
+    my_game = TicTacToe.TicTacToe(
+        number_of_human_players=2,
     )
     my_game.play_game()
